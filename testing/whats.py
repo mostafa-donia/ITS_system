4846f975d8d2538931c4ca7d6989cbe82568379d
Mostafa Donia <mostafa.tarek.donia@gmail.com>
2026-04-02 09:09:08 +0200

    done whatsapp

--- testing/whats.py
+++ testing/whats.py
@@ -4,36 +4,6 @@ from time import sleep
 from pyautogui import hotkey,press,write
 from pyperclip import paste,copy
 
-def prepare_phones(phones:str):
-    table = str.maketrans("٠١٢٣٤٥٦٧٨٩","0123456789")
-    phones = phones.translate(table)
-    phones = list(phones)
-    phone_list =""
-    for i,c in enumerate(phones) :
-        if c == "," and phones[i+1] == ",":
-            phones.pop(i+1)
-        phone_list+=(c)
-    phone_list = phone_list.split(",")
-    corrected_phone_list = []
-    for i in phone_list :
-        if len(i)>10 and len(i)<12 :
-            corrected_phone_list.append(i)
-        elif len(i)== 10 and i[0] == "1" :
-            corrected_phone_list.append("0"+i)
-        elif len(i)== 12 and i[0] =="2" :
-            i = i.replace("2","",1)
-            corrected_phone_list.append(i)
-    return corrected_phone_list
-
-def prepare_messages(messages:str):
-    messages = list(messages)
-    message_list =""
-    for i,c in enumerate(messages) :
-        if c == "," and messages[i+1] == ",":
-            messages.pop(i+1)
-        message_list+=(c)
-    message_list = message_list.split(",")
-    return message_list
 
 def whatsupp_ready():
     chrome_windows = [w for w in getAllWindows() if "Chrome" in w.title]
@@ -123,38 +93,11 @@ def send_message(phone:str,message:str):
     hotkey("ctrl", "v")
     press("enter")
 
-
-def sending(phones:list,messages:list):
-    if len(phones) == 0 or len(messages) == 0 :
-        print("No phones or messages to send")
-        return "No phones or messages to send"
-    if len(phones) != len(messages) and len(messages) != 1:
-        print("Number of phones and messages must be the same")
-        return "Number of phones and messages must be the same"
-    if len(messages) == 1 :
-        message = messages[0]
-        
-        for phone in phones :
-            if phone == phones[0] :
-                print(f"Sending first message to {phone}")
-                whatsupp_ready()
-            print(f"Sending {message} to {phone}")
-            send_message(phone,message)
-            sleep(0.1)
-    else :
-        for phone in phones :
-            if phone == phones[0] :
-                print(f"Sending first message to {phone}")
-                whatsupp_ready()
-            for message in messages :
-                print(f"Sending {message} to {phone}")
-                send_message(phone,message)
-                sleep(0.1)
-
 if __name__ == "__main__":
-    print(prepare_phones("01080922432,,201094832294,1097855504,,١٠٩٨٥٧٠٣٠٠"))
-    print(prepare_messages("01080922432,,201094832294,1097855504,,١٠٩٨٥٧٠٣٠٠"))
-    sending(prepare_phones("01080922432,,201094832294,1097855504,,١٠٩٨٥٧٠٣٠٠"), prepare_messages("01080922432,,201094832294,1097855504,,١٠٩٨٥٧٠٣٠٠"))
-
-
-
+    whatsupp_ready()
+    phones = ["01080922432","01098570300","01080922432","01098570300","01080922432","01098570300"]
+    message = "Hello World"
+    for phone in phones:
+        send_message(phone, message)
+        sleep(0.2)
+    
